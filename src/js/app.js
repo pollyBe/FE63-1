a2a2e1d49e6a4a3c74778ee640d936abda6d76af
PolinaBel <120118029+pollyBe@users.noreply.github.com>
2024-07-16 14:52:59 +0300

    add slick libr to the projrct

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+$(document).ready(function(){
+  $('.slider-wrapper').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
